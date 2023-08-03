3a763cbeeb39e2c4c19bf0d038f1916ef2b4e811
He Xiang Koh <kohhxx6@gmail.com>
2023-08-03 17:49:53 +0800

    Added userDetailService implmentation and userDetails implementation

---
+++ backend/src/main/java/com/avensys/loginsystem/userDetail/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.avensys.loginsystem.userDetail;
+
+import com.avensys.loginsystem.user.User;
+import com.avensys.loginsystem.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * This method is called by spring security to get the user details. If user is not found,
+     * it will throw an exception and the user will not be authenticated
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        return user.map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
